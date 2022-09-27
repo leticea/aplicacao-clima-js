f088802a38dc4e6c0d4aad61d3005cd8f17762ef
leticea <nascimento.leticeah@gmail.com>
2022-09-27 17:29:35 -0300

    criando o evento do botão search

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
-// c51de47c54cdbf0d89bfa4ba484a079b
+// [Variáveis e seleção de elementos]
+const apiKey = "c51de47c54cdbf0d89bfa4ba484a079b";
+const apiCountryURL = "https://countryflagsapi.com/png/";
 
+
+window.onload = function() {
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+// [Funções]
+const showWeatherData = (city) => {
+    console.log(city);
+}
+
+// [Eventos]
+searchBtn.addEventListener("click", (e) => {
+
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+
+    
+});
+};
 
