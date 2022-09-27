0fa8b0bcc9000621c6587dadf77f6e30a146f840
leticea <nascimento.leticeah@gmail.com>
2022-09-27 17:36:56 -0300

    criando os dados dos elementos

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,14 @@ window.onload = function() {
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
 // [Funções]
 const showWeatherData = (city) => {
     console.log(city);
