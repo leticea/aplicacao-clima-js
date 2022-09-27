bc9e1681dffcd0f2b00b5b23bb20ace24709cb6a
leticea <nascimento.leticeah@gmail.com>
2022-09-27 18:12:06 -0300

    inserindo os elementos para buscar os dados

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ const tempElement = document.querySelector("#temperature span");
 const descElement = document.querySelector("#description");
 const weatherIconElement = document.querySelector("#weather-icon");
 const countryElement = document.querySelector("#country");
-const umidityElement = document.querySelector("#umidity span");
+const humidityElement = document.querySelector("#humidity span");
 const windElement = document.querySelector("#wind span");
 
 // [Funções]
@@ -23,12 +23,19 @@ const getWeatherData = async(city) => {
     const res = await fetch(apiWeatherURL)
     const data = await res.json();
 
-    console.log(data)
+    return data;
 }
 
+const showWeatherData = async(city) => {
+    const data = await getWeatherData(city);
 
-const showWeatherData = (city) => {
-    getWeatherData(city);
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description;
+    weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src", apiCountryURL + data.sys.country);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
 }
 
 // [Eventos]
@@ -38,9 +45,7 @@ searchBtn.addEventListener("click", (e) => {
 
     const city = cityInput.value;
 
-    showWeatherData(city);
-
-    
+    showWeatherData(city);    
 });
 };
 
